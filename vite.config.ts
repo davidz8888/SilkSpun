e73836dd929db2e26514f2ae8bbe518f62229e46
davidz8888 <davidzhang561@gmail.com>
2025-02-18 15:44:59 -0800

    Added three and glsl dependency

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite';
+import glsl from 'vite-plugin-glsl';
 
 export default defineConfig({
   base: '/SilkSpun/', // Replace with your repository name (include leading/trailing slash)
+  plugins: [glsl()],
 });
